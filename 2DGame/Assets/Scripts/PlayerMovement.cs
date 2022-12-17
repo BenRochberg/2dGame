fc1607c4803b96f6fdb52690de185a6702548f73
Ben Rochberg <benrochberg@gmail.com>
2022-12-17 17:41:54 -0500

    Player Jump and Fall
    Player now has Jump and Fall animations.
    All animations are linked to an enum and use an int condition in the animator called "state."
    The animator sets the movement state to the different states by equaling the "state" int condition to the movement state number in the enum.

--- 2DGame/Assets/Scripts/PlayerMovement.cs
+++ 2DGame/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,12 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer sprite;      //Player's SpriteRenderer variable.
     private Animator anim;              //Player's Animator variable.
 
+    private enum MovementState { idle, running, jumping, falling }  //Enumeration for different movement states.
+
     [SerializeField] private float playerVelocity;      //Player's Jump Force, how high can the player jump.
     [SerializeField] private float playerSpeed;         //Player's Speed, how fast the player to run.
     [SerializeField] private int playerGravity;         //Player's Gravity, how strong is the player being pulled down.
     [SerializeField] private float dirX;                //Player's directional X value..
-
 
 
     private void Start()
@@ -43,20 +44,35 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateAnimations()
     {
+        MovementState state;
+
+        //Running animation
         if (dirX > 0f)                          //If the player is going right.
         {
-            anim.SetBool("Running", true);      //Set Running to True.
+            state = MovementState.running;      //Set local Movement State variable to Running.
             sprite.flipX = false;               //Used to flip the player's sprite on the X-axis once the player moves left.
         }
         else if (dirX < 0f)                     //If the player is going left.
         {
-            anim.SetBool("Running", true);      //Set Running to True.
+            state = MovementState.running;      //Set local Movement State variable to Running.
             sprite.flipX = true;                //Flip the player's sprite on the X-axis.
         }
         else                                    //If the player is Idle.
         {
-            anim.SetBool("Running", false);     //Set Running to False.
+            state = MovementState.idle;         //Set local Movement State variable to Idle.
         }
+
+        //Jumping animation
+        if (rb.velocity.y > .1f)                //If the player is above 0.1 on the Y-axis.
+        {
+            state = MovementState.jumping;      //Set local Movement State variable to Jumping.
+        }
+        else if (rb.velocity.y < -.1f)          //If the player is below -0.1 on the Y-axis.
+        {
+            state = MovementState.falling;      //Set local Movement State variable to Falling.
+        }
+
+        anim.SetInteger("state", (int)state);   //Setting the "state" int condition and converting the Movement State enum into an int.
     }
 
 }
