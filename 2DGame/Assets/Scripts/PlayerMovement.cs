52c82b81c387be3681266363b3802022858868f9
Ben Rochberg <benrochberg@gmail.com>
2022-12-17 10:25:39 -0500

    Player Movement
    Variables for the Player's rigidbody, player velocity, horizontal velocity, and player gravity.
    Player can now move left and right on the ground and while jumping.
    Using Input.GetButtonDown now.
    Made the Main Camera a child of the Player to make the camera follow the player.

---
+++ 2DGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;    //Player's Rigidbody2D variable.
+    public float playerVelocity; //Player's Jump Force, how high can the player jump.
+    public float horizontalVelocity;
+    public int playerGravity;  //Player's Gravity, how strong is the player being pulled down.
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //Getting the Player's Rigidbody2D component and equalling it to the Rigidbody2D variable.
+        rb.gravityScale = playerGravity;  //Setting the Player's rigidbody gravity scale equal to the playerGravity Int variable to edit in Inspector.
+    }
+
+    private void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * horizontalVelocity, rb.velocity.y);
+
+
+
+        //Jump Command
+        if (Input.GetButtonDown("Jump")) //Checking if the Player is pressing the Space Bar.
+        {
+            //Setting the Player's rigidbody velocity equal to the playerVelocity Int variable to edit in Inspector.
+            rb.velocity = new Vector2(rb.velocity.x, playerVelocity); //If Space is pressed, change the Player's Velocity.
+        }
+
+
+    }
+
+}
